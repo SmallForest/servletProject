6f89c50ab5249b4a27c996fbb2df47dd8ffd40e4
smallforest <1032817724@qq.com>
2022-06-02 13:34:58 +0800

    pojo

---
+++ src/main/java/com/yangsen/pojo/User.java
@@ -0,0 +1,172 @@
+package com.yangsen.pojo;
+
+import java.time.LocalDate;
+import java.time.Year;
+
+public class User {
+    private Integer id; //id
+    private String userCode; //用户编码
+    private String userName; //用户名称
+    private String userPassword; //用户密码
+    private Integer gender;  //性别
+    private LocalDate birthday;  //出生日期
+    private String phone;   //电话
+    private String address; //地址
+    private Integer userRole;    //用户角色
+    private Integer createdBy;   //创建者
+    private LocalDate creationDate; //创建时间
+    private Integer modifyBy;     //更新者
+    private LocalDate modifyDate;   //更新时间
+
+    public User() {
+    }
+
+    public User(Integer id, String userCode, String userName, String userPassword, Integer gender, LocalDate birthday, String phone, String address, Integer userRole, Integer createdBy, LocalDate creationDate, Integer modifyBy, LocalDate modifyDate) {
+        this.id = id;
+        this.userCode = userCode;
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.phone = phone;
+        this.address = address;
+        this.userRole = userRole;
+        this.createdBy = createdBy;
+        this.creationDate = creationDate;
+        this.modifyBy = modifyBy;
+        this.modifyDate = modifyDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Integer userRole) {
+        this.userRole = userRole;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Integer getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(Integer modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public LocalDate getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(LocalDate modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userCode='" + userCode + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", gender=" + gender +
+                ", birthday=" + birthday +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", userRole=" + userRole +
+                ", createdBy=" + createdBy +
+                ", creationDate=" + creationDate +
+                ", modifyBy=" + modifyBy +
+                ", modifyDate=" + modifyDate +
+                '}';
+    }
+
+    /**
+     * 获取年龄
+     *
+     * @return Integer
+     */
+    public Integer getAge() {
+        return Year.now().getValue() - birthday.getYear();
+    }
+}
+
